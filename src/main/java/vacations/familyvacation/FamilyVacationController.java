81bb2b9cdaa10a503f5208875b3506d510bdb62a
meyerCodeUniverse <36606751+meyerCodeUniverse@users.noreply.github.com>
2018-04-13 16:24:20 -0500

    added final jsps and table dependencies

--- src/main/java/vacations/familyvacation/FamilyVacationController.java
+++ src/main/java/vacations/familyvacation/FamilyVacationController.java
@@ -2,27 +2,32 @@ package vacations.familyvacation;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class FamilyVacationController {
+	@Autowired FamilyVacationDao dao;
 	
 	@RequestMapping(value="/form")
 	public ModelAndView familyvacation() {
 		ModelAndView modelAndView = new ModelAndView();
-		
-		modelAndView.setViewName("FamilyvacationForm");
-		modelAndView.addObject("FamilyVacation", new FamilyVacation());
-		
+		modelAndView.setViewName("familyvacationForm");
+		modelAndView.addObject("familyvacation", new FamilyVacation());
 		return modelAndView;
+		
+	
+		
 }
 
 	@RequestMapping(value = "/result")
 	public ModelAndView processFamilyVacation(FamilyVacation familyvacation) {
 		ModelAndView modelAndView = new ModelAndView();
-		// dao.insertFamilyVacation(familyvacation);
+		dao.insertFamilyVacation(familyvacation);
 		modelAndView.setViewName("familyvacationResult");
 		modelAndView.addObject("f", familyvacation);
 		return modelAndView;
@@ -38,9 +43,9 @@ public class FamilyVacationController {
 
 	}
 
-	// @Bean
-	// public FamilyVacationDao dao() {
-	// 	FamilyVacationDao bean = new FamilyVacationDao();
-	// 	return bean;
-	// }
+	@Bean
+	public FamilyVacationDao dao() {
+		FamilyVacationDao bean = new FamilyVacationDao();
+	 	return bean;
+	 }
 }
